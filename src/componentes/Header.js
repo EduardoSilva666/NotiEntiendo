f69ac0b96166f8d2f9ac31d70513c24413a213f1
EduardoSilva666 <90467089+EduardoSilva666@users.noreply.github.com>
2021-10-10 22:53:58 -0500

    NotiEntiendo
    Noticias

---
+++ src/componentes/Header.js
@@ -0,0 +1,14 @@
+  
+import React from 'react'
+
+const Header= () => {
+    return (
+        <nav class="navbar navbar-light ">
+            <a className="navbar-brand text-bold m-2" href="/" >
+                NotiEntiendo
+            </a>
+            
+        </nav>
+    )
+}
+export default Header;
